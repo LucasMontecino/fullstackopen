0862cd0abb99744563864d0b549dbb7ad8c5afe7
Lucas Montecino <lucasmontecino95@gmail.com>
2025-04-29 16:16:53 -0300

    feat: add Filter component for search functionality

---
+++ part2/phonebook/src/components/Filter.jsx
@@ -0,0 +1,14 @@
+const Filter = ({ search, handleSearch }) => {
+  return (
+    <div>
+      filter shown with{' '}
+      <input
+        type="text"
+        value={search}
+        onChange={handleSearch}
+      />
+    </div>
+  );
+};
+
+export default Filter;
