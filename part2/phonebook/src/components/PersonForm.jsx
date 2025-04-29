8914d767ae2c3bdc6c13132578d754a9cbe59575
Lucas Montecino <lucasmontecino95@gmail.com>
2025-04-29 16:16:59 -0300

    feat: add PersonForm component for adding new contacts

---
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -0,0 +1,37 @@
+const PersonForm = ({
+  addName,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange,
+}) => {
+  return (
+    <form onSubmit={addName}>
+      <div>
+        name:{' '}
+        <input
+          type="text"
+          id="new-name"
+          name="name"
+          value={newName}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        number:{' '}
+        <input
+          type="text"
+          id="new-phone"
+          name="phone"
+          value={newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+export default PersonForm;
