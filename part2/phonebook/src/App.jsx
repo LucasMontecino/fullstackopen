d30699f47b01aa777272f346fbf7e3059bf90ebb
Lucas Montecino <lucasmontecino95@gmail.com>
2025-04-29 16:27:21 -0300

    feat: initialize persons state and fetch data from API

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: '040-123456' },
-    {
-      id: 2,
-      name: 'Ada Lovelace',
-      number: '39-44-5323523',
-    },
-    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-    {
-      id: 4,
-      name: 'Mary Poppendieck',
-      number: '39-23-6423122',
-    },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
 
@@ -63,6 +51,18 @@ const App = () => {
     setNewName('');
     setNewNumber('');
   };
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        console.log('promise fulfilled');
+        setPersons(response.data);
+      });
+  }, []);
+
+  console.log('rendering...', persons.length, 'persons');
 
   return (
     <div>
