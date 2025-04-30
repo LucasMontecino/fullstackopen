7f88a86632c08c2a393a7b2438f23579855bd32d
Lucas Montecino <lucasmontecino95@gmail.com>
2025-04-29 21:05:40 -0300

    feat: add deletedPerson prop to Persons component for handling deletions

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,10 +1,14 @@
 import PersonDetails from './PersonDetails';
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, deletedPerson }) => {
   return (
     <div>
       {persons.map((person) => (
-        <PersonDetails key={person.id} person={person} />
+        <PersonDetails
+          key={person.id}
+          person={person}
+          onClick={() => deletedPerson(person)}
+        />
       ))}
     </div>
   );
