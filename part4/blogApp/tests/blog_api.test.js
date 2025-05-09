d5c771ebaf28fa3a4d54c47364c05da106b183eb
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-09 14:25:18 -0300

    test: add additional tests for blog API functionality

--- part4/blogApp/tests/blog_api.test.js
+++ part4/blogApp/tests/blog_api.test.js
@@ -33,6 +33,46 @@ test('all blogs are returned', async () => {
   );
 });
 
+test('unique identifier is correctly named id', async () => {
+  const blogs = await helper.blogsInDb();
+  const blogToView = Object.keys(blogs[0]);
+  assert(blogToView.includes('id'));
+});
+
+test('a specific blog is within the returned blogs', async () => {
+  const blogs = await helper.blogsInDb();
+  const titles = blogs.map((b) => b.title);
+
+  assert(titles.includes('Type wars'));
+});
+
+test('a valid blog can be added', async () => {
+  const newBlogs = {
+    title: 'Always separate app and server files!',
+    author: 'Nermine Slimane',
+    url: 'https://dev.to/nermine-slimane/always-separate-app-and-server-files--1nc7',
+    likes: 25,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlogs)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  assert.strictEqual(
+    blogsAtEnd.length,
+    helper.initialBlogs.length + 1
+  );
+
+  const titles = blogsAtEnd.map((b) => b.title);
+  assert(
+    titles.includes('Always separate app and server files!')
+  );
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
