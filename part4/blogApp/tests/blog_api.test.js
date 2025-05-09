c581c41393fc927940c185d160a79db54730fed2
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-09 17:39:04 -0300

    refactor: restructure blog API tests for improved organization and readability

--- part4/blogApp/tests/blog_api.test.js
+++ part4/blogApp/tests/blog_api.test.js
@@ -1,4 +1,9 @@
-const { test, after, beforeEach } = require('node:test');
+const {
+  test,
+  after,
+  beforeEach,
+  describe,
+} = require('node:test');
 const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
@@ -8,107 +13,185 @@ const helper = require('./test.helper');
 
 const api = supertest(app);
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
+describe('at the beginning some blogs are saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
 
-  for (const blog of helper.initialBlogs) {
-    const blogObject = new Blog(blog);
-    await blogObject.save();
-  }
-});
+    for (const blog of helper.initialBlogs) {
+      const blogObject = new Blog(blog);
+      await blogObject.save();
+    }
+  });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test('all blogs are returned', async () => {
-  const blogs = await helper.blogsInDb();
+  test('all blogs are returned', async () => {
+    const blogs = await helper.blogsInDb();
 
-  assert.strictEqual(
-    blogs.length,
-    helper.initialBlogs.length
-  );
-});
+    assert.strictEqual(
+      blogs.length,
+      helper.initialBlogs.length
+    );
+  });
 
-test('unique identifier is correctly named id', async () => {
-  const blogs = await helper.blogsInDb();
-  const blogToView = Object.keys(blogs[0]);
-  assert(blogToView.includes('id'));
-});
+  describe('view some blogs', () => {
+    test('unique identifier is correctly named id', async () => {
+      const blogs = await helper.blogsInDb();
+      const blogToView = Object.keys(blogs[0]);
+      assert(blogToView.includes('id'));
+    });
+    test('a specific blog is within the returned blogs', async () => {
+      const blogs = await helper.blogsInDb();
+      const titles = blogs.map((b) => b.title);
 
-test('a specific blog is within the returned blogs', async () => {
-  const blogs = await helper.blogsInDb();
-  const titles = blogs.map((b) => b.title);
+      assert(titles.includes('Type wars'));
+    });
+  });
 
-  assert(titles.includes('Type wars'));
-});
+  describe('added new blogs', () => {
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        title: 'Always separate app and server files!',
+        author: 'Nermine Slimane',
+        url: 'https://dev.to/nermine-slimane/always-separate-app-and-server-files--1nc7',
+        likes: 25,
+      };
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Always separate app and server files!',
-    author: 'Nermine Slimane',
-    url: 'https://dev.to/nermine-slimane/always-separate-app-and-server-files--1nc7',
-    likes: 25,
-  };
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      const blogsAtEnd = await helper.blogsInDb();
 
-  const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(
+        blogsAtEnd.length,
+        helper.initialBlogs.length + 1
+      );
 
-  assert.strictEqual(
-    blogsAtEnd.length,
-    helper.initialBlogs.length + 1
-  );
+      const titles = blogsAtEnd.map((b) => b.title);
+      assert(
+        titles.includes(
+          'Always separate app and server files!'
+        )
+      );
+    });
 
-  const titles = blogsAtEnd.map((b) => b.title);
-  assert(
-    titles.includes('Always separate app and server files!')
-  );
-});
+    test('added a new blog without likes gives a value of zero', async () => {
+      const newBlog = {
+        title: 'Clean Architecture on Frontend',
+        author: 'Alex Bespoyasov',
+        url: 'https://dev.to/bespoyasov/clean-architecture-on-frontend-4311',
+      };
 
-test('added a new blog without likes gives a value of zero', async () => {
-  const newBlog = {
-    title: 'Clean Architecture on Frontend',
-    author: 'Alex Bespoyasov',
-    url: 'https://dev.to/bespoyasov/clean-architecture-on-frontend-4311',
-  };
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+      const blogsAtEnd = await helper.blogsInDb();
+      const blogToView = blogsAtEnd.at(-1);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  const blogToView = blogsAtEnd.at(-1);
+      assert.strictEqual(blogToView.likes, 0);
+    });
 
-  assert.strictEqual(blogToView.likes, 0);
-});
+    test('attempted add a blog without title or url is forbidden', async () => {
+      const newBlog = {
+        author: 'Lucas',
+        likes: 300,
+      };
 
-test('attempted add a blog without title or url is forbidden', async () => {
-  const newBlog = {
-    author: 'Lucas',
-    likes: 300,
-  };
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+        .expect('Content-Type', /application\/json/);
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    .expect('Content-Type', /application\/json/);
+      const blogsAtEnd = await helper.blogsInDb();
+      assert.strictEqual(
+        blogsAtEnd.length,
+        helper.initialBlogs.length
+      );
+    });
+  });
 
-  const blogsAtEnd = await helper.blogsInDb();
-  assert.strictEqual(
-    blogsAtEnd.length,
-    helper.initialBlogs.length
-  );
+  describe('deletion of a note', () => {
+    test('delete blog with valid id', async () => {
+      const blogs = await helper.blogsInDb();
+      const blogToDelete = blogs[0];
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204);
+
+      const blogsAtEnd = await helper.blogsInDb();
+
+      const titles = blogsAtEnd.map((b) => b.title);
+
+      assert(!titles.includes(blogToDelete.title));
+
+      assert.strictEqual(
+        blogsAtEnd.length,
+        helper.initialBlogs.length - 1
+      );
+    });
+
+    test('failed with status 404 if blog does not exist', async () => {
+      const validateNonexistId =
+        await helper.nonExistingId();
+
+      await api
+        .delete(`/api/blogs/${validateNonexistId}`)
+        .expect(404);
+    });
+
+    test('failed with status 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445';
+
+      await api
+        .delete(`/api/blogs/${invalidId}`)
+        .expect(400);
+    });
+  });
+
+  describe('updating a note', () => {
+    test('updated specific blog', async () => {
+      const blogs = await helper.blogsInDb();
+      const blogToUpdate = blogs[0];
+
+      const toUpdate = { likes: 12 };
+
+      blogToUpdate.likes = toUpdate.likes;
+
+      await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(blogToUpdate)
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+    });
+
+    test('failed with status 404 if blog does not exist', async () => {
+      const validateNonexistId =
+        await helper.nonExistingId();
+
+      await api
+        .put(`/api/blogs/${validateNonexistId}`)
+        .expect(404);
+    });
+
+    test('failed with status 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445';
+
+      await api.put(`/api/blogs/${invalidId}`).expect(400);
+    });
+  });
 });
 
 after(async () => {
