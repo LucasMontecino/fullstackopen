e2c1a38c03b8a1e072ff3416f799caf6f32e54c1
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-09 13:44:52 -0300

    feat: add initial tests for blog API endpoints

---
+++ part4/blogApp/tests/blog_api.test.js
@@ -0,0 +1,38 @@
+const { test, after, beforeEach } = require('node:test');
+const assert = require('node:assert');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const helper = require('./test.helper');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (const blog of helper.initialBlogs) {
+    const blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+});
+
+test('all blogs are returned', async () => {
+  const blogs = await helper.blogsInDb();
+
+  assert.strictEqual(
+    blogs.length,
+    helper.initialBlogs.length
+  );
+});
+
+after(async () => {
+  await mongoose.connection.close();
+});
