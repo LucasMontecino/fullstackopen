1f828671bf0275758fc28938560a80cb55a34aeb
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-09 16:12:25 -0300

    refactor: simplify error handling in GET and POST endpoints

--- part4/blogApp/controllers/blogs.js
+++ part4/blogApp/controllers/blogs.js
@@ -1,16 +1,12 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', async (request, response, next) => {
-  try {
-    const blogs = await Blog.find({});
-    return response.json(blogs);
-  } catch (error) {
-    next(error);
-  }
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  return response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body;
 
   if (!body.likes) {
@@ -18,12 +14,18 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 
   const blog = new Blog(body);
-  try {
-    const savedBlog = await blog.save();
-    return response.status(201).json(savedBlog);
-  } catch (error) {
-    next(error);
-  }
+  const savedBlog = await blog.save();
+  return response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+  const deletedBlog = await Blog.findByIdAndDelete(id);
+  if (!deletedBlog)
+    return response
+      .status(404)
+      .send(`No resource find with id ${id}`);
+  else return response.status(204).end;
 });
 
 module.exports = blogsRouter;
