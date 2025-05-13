9a57db3fe4c1af5ac1e512af054f6f999a580e55
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-13 16:01:06 -0300

    fix: add expiration time to JWT token in login route

--- part4/blogApp/controllers/login.js
+++ part4/blogApp/controllers/login.js
@@ -25,7 +25,9 @@ loginRouter.post('/', async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, config.SECRET);
+  const token = jwt.sign(userForToken, config.SECRET, {
+    expiresIn: 60 * 60,
+  });
 
   return res.json({
     token,
