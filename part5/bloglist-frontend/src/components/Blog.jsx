c9990de1190c3b76e92f191c6d384934a49b615a
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-15 11:02:02 -0300

    feat: refactor Blog component to include details toggle and update functionality

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,49 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
+import Button from './Button';
 
-export default Blog
+const Blog = ({ blog, updateBlog, children }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () =>
+    setShowDetails((prev) => !prev);
+
+  return (
+    <div
+      className="blog"
+      style={{
+        border: '1px solid #000',
+        padding: '2px 6px',
+      }}
+    >
+      {blog.title} {blog.author}
+      <Button
+        type={'button'}
+        label={!showDetails ? 'view' : 'hide'}
+        onClick={toggleDetails}
+      />
+      {showDetails && (
+        <div>
+          <a
+            href={blog.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {blog.url}
+          </a>
+          <div>
+            likes {blog.likes}{' '}
+            <Button
+              type={'button'}
+              label={'like'}
+              onClick={updateBlog}
+            />
+          </div>
+          <div>{blog.user?.name ?? 'unknown creator'}</div>
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
