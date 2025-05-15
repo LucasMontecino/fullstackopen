55b48b96290e73802788ee177f058a1c71c52cbe
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-15 11:02:39 -0300

    feat: add update and deleteItem functions to blogs service

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -23,4 +23,38 @@ const create = async (blog) => {
   return response.data;
 };
 
-export default { getAll, create, setToken };
+const update = async (id, blog) => {
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  const response = await axios.put(
+    `${baseUrl}/${id}`,
+    blog,
+    config
+  );
+  return response.data;
+};
+
+const deleteItem = async (id) => {
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  const response = await axios.delete(
+    `${baseUrl}/${id}`,
+    config
+  );
+
+  return response.data;
+};
+
+export default {
+  getAll,
+  create,
+  setToken,
+  update,
+  deleteItem,
+};
