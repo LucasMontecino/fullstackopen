4cc466e8492ac6a7ab4019663bf9296431e149c8
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-15 11:02:22 -0300

    feat: refactor CreateBlog component to manage state internally and use Button for submission

--- part5/bloglist-frontend/src/components/CreateBlog.jsx
+++ part5/bloglist-frontend/src/components/CreateBlog.jsx
@@ -1,47 +1,70 @@
+import { useState } from 'react';
 import InputText from './InputText';
+import Button from './Button';
 
-const CreateBlog = ({
-  title,
-  author,
-  url,
-  handleChange,
-  handleSubmit,
-}) => {
+const CreateBlog = ({ createBlog }) => {
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: '',
+  });
+
+  const handleNewBlog = (e) => {
+    setNewBlog((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const addBlog = async (e) => {
+    e.preventDefault();
+
+    await createBlog(newBlog);
+
+    setNewBlog({
+      title: '',
+      author: '',
+      url: '',
+    });
+  };
+
   return (
     <div>
       <h2>create new</h2>
-      <form onSubmit={handleSubmit}>
-        <InputText
-          htmlFor={'title'}
-          label={'title:'}
-          name={'title'}
-          id={'title'}
-          value={title}
-          onChange={handleChange}
-          autoComplete={'title'}
-          type={'text'}
-        />
-        <InputText
-          htmlFor={'author'}
-          label={'author:'}
-          name={'author'}
-          id={'author'}
-          value={author}
-          onChange={handleChange}
-          autoComplete={'author'}
-          type={'text'}
-        />
-        <InputText
-          htmlFor={'url'}
-          label={'url:'}
-          name={'url'}
-          id={'url'}
-          value={url}
-          onChange={handleChange}
-          autoComplete={'url'}
-          type={'text'}
-        />
-        <button type="submit">create</button>
+      <form onSubmit={addBlog}>
+        <div style={{ marginBottom: '6px' }}>
+          <InputText
+            htmlFor={'title'}
+            label={'title:'}
+            name={'title'}
+            id={'title'}
+            value={newBlog.title}
+            onChange={handleNewBlog}
+            autoComplete={'off'}
+            type={'text'}
+          />
+          <InputText
+            htmlFor={'author'}
+            label={'author:'}
+            name={'author'}
+            id={'author'}
+            value={newBlog.author}
+            onChange={handleNewBlog}
+            autoComplete={'off'}
+            type={'text'}
+          />
+          <InputText
+            htmlFor={'url'}
+            label={'url:'}
+            name={'url'}
+            id={'url'}
+            value={newBlog.url}
+            onChange={handleNewBlog}
+            autoComplete={'url'}
+            type={'text'}
+          />
+        </div>
+        <Button type={'submit'} label={'create'} />
       </form>
     </div>
   );
