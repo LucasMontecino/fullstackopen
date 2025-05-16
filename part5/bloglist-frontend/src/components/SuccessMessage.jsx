4fbf0d3340576263de59fba30fff1dec179cb37b
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-16 09:53:58 -0300

    refactor: remove SuccessMessage component as it is no longer needed

--- part5/bloglist-frontend/src/components/SuccessMessage.jsx
+++
@@ -1,9 +0,0 @@
-const SuccessMessage = ({ message }) => {
-  return (
-    message !== null && (
-      <p style={{ color: 'green' }}>{message}</p>
-    )
-  );
-};
-
-export default SuccessMessage;
