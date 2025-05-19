94ac2af023dc522e6c09f5712bee3ac030cd63cc
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-19 15:56:07 -0300

    refactor: simplify anecdote initialization in App component by using initializeAnecdotes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,18 +2,14 @@ import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Notification from './components/Notification';
 import { useDispatch } from 'react-redux';
-import anecdoteService from './services/anecdotes';
 import { useEffect } from 'react';
-import { setAnecdotes } from './reducers/anecdoteReducer';
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    (async () => {
-      const anecdotes = await anecdoteService.getAll();
-      dispatch(setAnecdotes(anecdotes));
-    })();
+    dispatch(initializeAnecdotes());
   }, []);
 
   return (
