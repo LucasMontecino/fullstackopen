5b0805ad2c54db497569a6224f29e42d74fec2f6
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-19 10:38:31 -0300

    feat: add notificationReducer for managing notification state

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,23 @@
+export const notificationReducer = (state = '', action) => {
+  switch (action.type) {
+    case 'SET_NOTIFICATION':
+      return action.payload;
+    case 'CLEAR_NOTIFICATION':
+      return '';
+    default:
+      return state;
+  }
+};
+
+export const setNotification = (message) => {
+  return {
+    type: 'SET_NOTIFICATION',
+    payload: message,
+  };
+};
+
+export const clearNotification = () => {
+  return {
+    type: 'CLEAR_NOTIFICATION',
+  };
+};
