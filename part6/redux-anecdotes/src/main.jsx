01e38a859ed5dbc9dbf214964bb6e7949ab87ed4
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-19 15:16:04 -0300

    refactor: simplify store setup by importing pre-configured store

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,22 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import {
-  legacy_createStore as createStore,
-  combineReducers,
-} from 'redux';
+import { store } from './store';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import { anecdoteReducer } from './reducers/anecdoteReducer';
-import { filterAnecdotesReducer } from './reducers/filterAnecdotesReducer';
-import { notificationReducer } from './reducers/notificationReducer';
-
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterAnecdotesReducer,
-  notification: notificationReducer,
-});
-
-const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
