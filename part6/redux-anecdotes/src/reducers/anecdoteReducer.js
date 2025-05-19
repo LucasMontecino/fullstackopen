b0e64fa052dfd544bf97e30b67703d6c1e5da1c7
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-19 15:56:13 -0300

    feat: add initializeAnecdotes function to fetch and set anecdotes from the service

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import anecdoteService from '../services/anecdotes';
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -26,4 +27,12 @@ export const {
   createAnecdote,
   setAnecdotes,
 } = anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
 export default anecdoteSlice.reducer;
