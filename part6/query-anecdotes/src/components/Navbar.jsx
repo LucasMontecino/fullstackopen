957074af447039a00b5f72ed85e14767b4f94f05
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-20 10:18:58 -0300

    feat: add Navbar component for navigation

---
+++ part6/query-anecdotes/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <nav className="container">
+      <h3>
+        <a
+          href="/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Anecdote app
+        </a>
+      </h3>
+    </nav>
+  );
+};
+
+export default Navbar;
