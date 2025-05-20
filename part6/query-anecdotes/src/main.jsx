78f64dec8da40912d50690748031785947c6fc74
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-20 17:44:58 -0300

    feat: wrap App component with NotificationContextProvider for context management

--- part6/query-anecdotes/src/main.jsx
+++ part6/query-anecdotes/src/main.jsx
@@ -5,11 +5,14 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query';
 import './index.css';
+import { NotificationContextProvider } from './context/NotificationContext';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <NotificationContextProvider>
+      <App />
+    </NotificationContextProvider>
   </QueryClientProvider>
 );
