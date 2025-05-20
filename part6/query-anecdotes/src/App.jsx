f34116666b243270de6b70eb32d4f7066242bab8
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-20 10:18:35 -0300

    refactor: simplify App component by removing unused code and integrating AnecdotesList

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,79 +1,17 @@
 import AnecdoteForm from './components/AnecdoteForm';
+import AnecdotesList from './components/AnecdotesList';
+import Navbar from './components/Navbar';
 import Notification from './components/Notification';
-import {
-  getAnecdotes,
-  updateAnecdote,
-} from './services/requests';
-import {
-  useQuery,
-  useQueryClient,
-  useMutation,
-} from '@tanstack/react-query';
 
 const App = () => {
-  const queryClient = useQueryClient();
-
-  const { isPending, isError, data } = useQuery({
-    queryKey: ['anecdotes'],
-    queryFn: getAnecdotes,
-    refetchOnWindowFocus: false,
-    retry: false,
-  });
-
-  const updateAnecdoteMutation = useMutation({
-    mutationFn: updateAnecdote,
-    onSuccess: (updatedAnecdote) => {
-      const anecdotes = queryClient.getQueryData([
-        'anecdotes',
-      ]);
-      queryClient.setQueryData(
-        ['anecdotes'],
-        anecdotes.map((anecdote) =>
-          anecdote.id === updatedAnecdote.id
-            ? { ...anecdote, votes: anecdote.votes + 1 }
-            : anecdote
-        )
-      );
-    },
-  });
-
-  const handleVote = (anecdote) => {
-    updateAnecdoteMutation.mutate(anecdote);
-  };
-
-  if (isError) {
-    return (
-      <p>
-        anecdote service not available due to problems in
-        server
-      </p>
-    );
-  }
-
-  if (isPending) {
-    return <p>loading data...</p>;
-  }
-
-  const anecdotes = data;
-
   return (
     <div>
-      <h3>Anecdote app</h3>
+      <Navbar />
 
       <Notification />
       <AnecdoteForm />
 
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>
-              vote
-            </button>
-          </div>
-        </div>
-      ))}
+      <AnecdotesList />
     </div>
   );
 };
