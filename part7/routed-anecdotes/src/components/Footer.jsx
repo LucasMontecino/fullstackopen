9edcc17e7f7ac271c8e39b550f6198bb6c0f006c
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-23 09:04:24 -0300

    feat: create Footer component

---
+++ part7/routed-anecdotes/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+const Footer = () => (
+  <div>
+    Anecdote app for{' '}
+    <a
+      href="https://fullstackopen.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Full Stack Open
+    </a>
+    . See{' '}
+    <a
+      href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js
+    </a>{' '}
+    for the source code.
+  </div>
+);
+
+export default Footer;
