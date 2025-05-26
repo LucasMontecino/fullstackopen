748d039f1228167a75c0121b83db5b1f87f73386
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-26 15:09:59 -0300

    chore: format blogs and login

---
+++ part7/bloglist/frontend/src/services/blogs.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+const baseUrl = '/api/blogs';
+
+let token = null;
+
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
+const getAll = async () => {
+  const request = await axios.get(baseUrl);
+  return request.data;
+};
+
+const create = async (blog) => {
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+
+  const response = await axios.post(baseUrl, blog, config);
+  return response.data;
+};
+
+const update = async (id, blog) => {
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  const response = await axios.put(`${baseUrl}/${id}`, blog, config);
+  return response.data;
+};
+
+const deleteItem = async (id) => {
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  const response = await axios.delete(`${baseUrl}/${id}`, config);
+
+  return response.data;
+};
+
+export default {
+  getAll,
+  create,
+  setToken,
+  update,
+  deleteItem,
+};
