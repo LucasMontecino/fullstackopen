26307039ef2318e83001c19b993268bad84bd59e
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-26 18:38:33 -0300

    feat: add query client provider and context to whole app

--- part7/bloglist-react-query-and-context/frontend/src/main.jsx
+++ part7/bloglist-react-query-and-context/frontend/src/main.jsx
@@ -1,5 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { BlogsContextProvider } from './context/blogsContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <BlogsContextProvider>
+      <App />
+    </BlogsContextProvider>
+  </QueryClientProvider>
+);
