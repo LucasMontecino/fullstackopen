a7062a0b715159b1781a293dbf1a337fe3a58794
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-27 15:27:50 -0300

    Revert "feat: add query client provider and context to whole app"
    This reverts commit 26307039ef2318e83001c19b993268bad84bd59e.

--- part7/bloglist-react-query-and-context/frontend/src/main.jsx
+++ part7/bloglist-react-query-and-context/frontend/src/main.jsx
@@ -1,15 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BlogsContextProvider } from './context/blogsContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
-    <BlogsContextProvider>
-      <App />
-    </BlogsContextProvider>
-  </QueryClientProvider>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
