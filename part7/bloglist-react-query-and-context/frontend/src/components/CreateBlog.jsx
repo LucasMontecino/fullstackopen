2eef87212e22c73491a00fe53767e30c4c4b903a
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-28 11:23:22 -0300

    feat: update CreateBlog now is using react query and context

--- part7/bloglist-react-query-and-context/frontend/src/components/CreateBlog.jsx
+++ part7/bloglist-react-query-and-context/frontend/src/components/CreateBlog.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import InputText from './InputText';
 import Button from './Button';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import blogService from '../services/blogs';
+import { setNotifications, setErrors } from '../utils';
+import { BlogsContext } from '../context/BlogsContext';
 
-const CreateBlog = ({ createBlog }) => {
+const CreateBlog = ({ handleAddBlog }) => {
   const [newBlog, setNewBlog] = useState({
     title: '',
     author: '',
     url: '',
+  });
+
+  const { dispatch } = useContext(BlogsContext);
+
+  const queryClient = useQueryClient();
+
+  const newBlogMutation = useMutation({
+    mutationFn: blogService.create,
+    onSuccess: (newBlog) => {
+      handleAddBlog();
+      setNewBlog({
+        title: '',
+        author: '',
+        url: '',
+      });
+      const blogs = queryClient.getQueryData(['blogs']);
+      queryClient.setQueryData(['blogs'], [...blogs, newBlog]);
+      setNotifications(
+        dispatch,
+        `a new blog ${newBlog.title} by ${newBlog.author} added`
+      );
+    },
+    onError: (error) => {
+      console.error({ error: error.response?.data?.error ?? error.message });
+      setErrors(dispatch, error.response?.data?.error ?? error.message);
+    },
   });
 
   const handleNewBlog = (e) => {
@@ -19,13 +49,7 @@ const CreateBlog = ({ createBlog }) => {
   const addBlog = async (e) => {
     e.preventDefault();
 
-    await createBlog(newBlog);
-
-    setNewBlog({
-      title: '',
-      author: '',
-      url: '',
-    });
+    newBlogMutation.mutate(newBlog);
   };
 
   return (
