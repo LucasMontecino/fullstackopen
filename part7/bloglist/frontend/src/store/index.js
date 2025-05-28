6b8a7f54ee0272baecca2046c89a2f61a56e7f5d
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-28 19:03:20 -0300

    feat: update configStore with new reducers

--- part7/bloglist/frontend/src/store/index.js
+++ part7/bloglist/frontend/src/store/index.js
@@ -3,12 +3,18 @@ import notificationReducer from './reducers/notificationReducer';
 import blogsReducer from './reducers/blogsReducer';
 import errorReducer from './reducers/errorReducer';
 import userReducer from './reducers/userReducer';
+import usersReducer from './reducers/usersReducer';
+import userDetailsReducer from './reducers/userDetailsReducer';
+import blogDetailsReducer from './reducers/blogDetailsReducer';
 
 const reducer = {
   notification: notificationReducer,
   blogs: blogsReducer,
   error: errorReducer,
   user: userReducer,
+  users: usersReducer,
+  userDetails: userDetailsReducer,
+  blogDetails: blogDetailsReducer,
 };
 
 export const store = configureStore({ reducer });
