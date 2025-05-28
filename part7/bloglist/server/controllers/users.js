8fb6fa294db1b24143ee2cce0e6df15f03222837
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-28 19:07:39 -0300

    refactor: updating user controllers

--- part7/bloglist/server/controllers/users.js
+++ part7/bloglist/server/controllers/users.js
@@ -10,4 +10,14 @@ usersRouter.get('/', async (req, res) => {
   return res.json(users);
 });
 
+usersRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id).populate('blogs', {
+    url: 1,
+    title: 1,
+    author: 1,
+  });
+  return res.json(user);
+});
+
 module.exports = usersRouter;
