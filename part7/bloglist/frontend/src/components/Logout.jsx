c9bbb90ef924b42449d769293a4e5a543a141432
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-28 19:01:53 -0300

    refactor: update logout component to handle its own functionality

--- part7/bloglist/frontend/src/components/Logout.jsx
+++ part7/bloglist/frontend/src/components/Logout.jsx
@@ -1,11 +1,17 @@
+import { logOut } from '../store/reducers/userReducer';
 import Button from './Button';
+import { useDispatch } from 'react-redux';
 
-const Logout = ({ handleClick }) => {
+const Logout = () => {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
   return (
     <Button
       type={'button'}
       label={'logout'}
-      onClick={handleClick}
+      onClick={handleLogout}
       testid={'logout'}
     />
   );
