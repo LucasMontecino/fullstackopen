f737761b004356e8513dd76fb6e87be960fd4d74
Lucas Montecino <lucasmontecino95@gmail.com>
2025-05-30 15:23:48 -0300

    refactor: handle the user session expiration

--- part7/bloglist/frontend/src/store/reducers/userReducer.js
+++ part7/bloglist/frontend/src/store/reducers/userReducer.js
@@ -1,12 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 import blogService from '../../services/blogs';
 import loginService from '../../services/login';
+import { jwtDecode } from 'jwt-decode';
 
 const initialState = () => {
   const loggedUserJSON = window.localStorage.getItem('loggedUser');
   if (loggedUserJSON) {
     const user = JSON.parse(loggedUserJSON);
+
+    if (Date.now() > user.expiration) {
+      window.localStorage.removeItem('loggedUser');
+      return null;
+    }
+
     blogService.setToken(user.token);
+
     return user;
   } else {
     return null;
@@ -33,8 +41,27 @@ export default userSlice.reducer;
 export const loginUser = (credentials) => {
   return async (dispatch) => {
     const result = await loginService.login(credentials);
-    dispatch(setUser(result));
-    window.localStorage.setItem('loggedUser', JSON.stringify(result));
+    const decodedToken = jwtDecode(result.token);
+
+    const expiration = decodedToken.exp * 1000;
+
+    const userWithExpiration = {
+      ...result,
+      expiration,
+    };
+
+    dispatch(setUser(userWithExpiration));
     blogService.setToken(result.token);
+
+    window.localStorage.setItem(
+      'loggedUser',
+      JSON.stringify(userWithExpiration)
+    );
+
+    const timeout = expiration - Date.now();
+    setTimeout(() => {
+      dispatch(logOut());
+      window.localStorage.removeItem('loggedUser');
+    }, timeout);
   };
 };
