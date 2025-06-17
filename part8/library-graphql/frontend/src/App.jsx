3c6864440321402b840dcee951668fcf7abeefc1
Lucas Montecino <lucasmontecino95@gmail.com>
2025-06-17 12:26:10 -0300

    feat: app template

--- part8/library-graphql/frontend/src/App.jsx
+++ part8/library-graphql/frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import Authors from './components/Authors';
+import Books from './components/Books';
+import NewBook from './components/NewBook';
+import Login from './components/Login';
+import Recommend from './components/Recommend';
 
 const App = () => {
-  return <Authors />;
+  return (
+    <Routes>
+      <Route path="/" element={<Authors />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/books" element={<Books />} />
+      <Route path="/add-book" element={<NewBook />} />
+      <Route path="/recommend" element={<Recommend />} />
+    </Routes>
+  );
 };
 
 export default App;
