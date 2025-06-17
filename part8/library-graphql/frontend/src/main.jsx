a286d3049de5a2cafa811ec1fe1edacefb150647
Lucas Montecino <lucasmontecino95@gmail.com>
2025-06-17 12:26:24 -0300

    feat: main config

--- part8/library-graphql/frontend/src/main.jsx
+++ part8/library-graphql/frontend/src/main.jsx
@@ -1,38 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Books from './components/Books.jsx';
-import NewBook from './components/NewBook.jsx';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import { Container } from 'react-bootstrap';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { NotificationsContextProvider } from './context/NotificationsContext.jsx';
 import ErrorNotification from './components/ErrorNotification.jsx';
 import Notification from './components/Notification.jsx';
+import { UserContextProvider } from './context/UserContext.jsx';
+import { TokenContextProvider } from './context/TokenContext.jsx';
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <NotificationsContextProvider>
-        <BrowserRouter>
-          <Navbar />
-          <Container className="mt-3">
-            <Notification />
-            <ErrorNotification />
-            <Routes>
-              <Route path="/" element={<App />} />
-              <Route path="/books" element={<Books />} />
-              <Route path="/add-book" element={<NewBook />} />
-            </Routes>
-          </Container>
-        </BrowserRouter>
-      </NotificationsContextProvider>
+      <TokenContextProvider>
+        <UserContextProvider>
+          <NotificationsContextProvider>
+            <BrowserRouter>
+              <Navbar />
+              <Container className="mt-3">
+                <Notification />
+                <ErrorNotification />
+                <App />
+              </Container>
+            </BrowserRouter>
+          </NotificationsContextProvider>
+        </UserContextProvider>
+      </TokenContextProvider>
     </ApolloProvider>
   </React.StrictMode>
 );
