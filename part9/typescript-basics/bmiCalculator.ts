3eb52c51817bcaf33de17aa680e3e641acf772bd
Lucas Montecino <lucasmontecino95@gmail.com>
2025-07-06 08:40:23 -0300

    feat: finish tasks 9.4 and 9.5

--- part9/typescript-basics/bmiCalculator.ts
+++ part9/typescript-basics/bmiCalculator.ts
@@ -5,17 +5,28 @@ interface BmiValues {
   weight: number;
 }
 
-const parseArguments = (args: string[]): BmiValues => {
-  if (args.length > 4) throw new Error('too many arguments');
-  if (args.length < 4) throw new Error('not enough arguments');
+export const parseArguments = (args: string[]): BmiValues => {
+  if (require.main === module) {
+    if (args.length > 4) throw new Error('too many arguments');
+    if (args.length < 4) throw new Error('not enough arguments');
 
-  if (isNotNumber(args[2]) || isNotNumber(args[3])) {
-    throw new Error('arguments must be numbers');
+    if (isNotNumber(args[2]) || isNotNumber(args[3])) {
+      throw new Error('arguments must be numbers');
+    } else {
+      return {
+        height: Number(args[2]),
+        weight: Number(args[3]),
+      };
+    }
   } else {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    };
+    if (isNotNumber(args[0]) || isNotNumber(args[1])) {
+      throw new Error('malformatted parameters');
+    } else {
+      return {
+        height: Number(args[0]),
+        weight: Number(args[1]),
+      };
+    }
   }
 };
 
@@ -30,14 +41,16 @@ export const calculateBmi = (height: number, weight: number): string => {
     : 'Obese';
 };
 
-try {
-  const { height, weight } = parseArguments(process.argv);
-  const result = calculateBmi(height, weight);
-  console.log(result);
-} catch (error: unknown) {
-  let errMsg = 'Sth went wrong: ';
-  if (error instanceof Error) {
-    errMsg += error.message;
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArguments(process.argv);
+    const result = calculateBmi(height, weight);
+    console.log(result);
+  } catch (error: unknown) {
+    let errMsg = 'Sth went wrong: ';
+    if (error instanceof Error) {
+      errMsg += error.message;
+    }
+    console.error({ error: errMsg });
   }
-  console.error({ error: errMsg });
 }
--- part9/typescript-basics/index.ts
+++ part9/typescript-basics/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { parseArguments } from './bmiCalculator';
 
 const app = express();
 
@@ -9,15 +10,35 @@ app.get('/hello', (_req, res) => {
 });
 
 app.get('/bmi', (req, res) => {
-  const { height, weight } = req.query;
+  const query = req.query;
 
-  const result = calculateBmi(Number(height), Number(weight));
+  try {
+    if (
+      query.height &&
+      typeof query.height === 'string' &&
+      query.weight &&
+      typeof query.weight === 'string'
+    ) {
+      const { height, weight } = parseArguments([query.height, query.weight]);
 
-  res.json({ weight, height, bmi: result });
+      const result = calculateBmi(height, weight);
+
+      res.json({ weight, height, bmi: result });
+      return;
+    } else {
+      throw new Error('malformatted parameters');
+    }
+  } catch (error: unknown) {
+    let errMsg: string = 'Sth went wrong: ';
+    if (error instanceof Error) {
+      errMsg += error.message;
+    }
+    res.status(400).json({ error: errMsg });
+  }
 });
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}/hello`);
+  console.log(`Server listening on port http://localhost:${PORT}`);
 });
