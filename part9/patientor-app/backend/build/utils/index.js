d27867092ce1b953b2698de8fb01a8f10f994d30
Lucas Montecino <lucasmontecino95@gmail.com>
2025-07-11 20:17:23 -0300

    chore: delete build folder

--- part9/patientor-app/backend/build/utils/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toNewPatient = void 0;
-const schemas_1 = require("../schemas");
-const toNewPatient = (obj) => {
-    return schemas_1.NewPatientSchema.parse(obj);
-};
-exports.toNewPatient = toNewPatient;
